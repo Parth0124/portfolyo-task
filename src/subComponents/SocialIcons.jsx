7b4276f921521778709258592ac7135416f01bb6
Abhiraj412 <abhangparth@gmail.com>
2024-04-02 02:21:55 +0530

    commit

--- src/subComponents/SocialIcons.jsx
+++ src/subComponents/SocialIcons.jsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion";
 import React from "react";
-// import { NavLink } from 'react-router-dom'
 import styled from "styled-components";
 import { LinkedIn, Github, Twitter, LeetCode } from "../components/AllSvgs";
 import { DarkTheme } from "../components/Themes";
