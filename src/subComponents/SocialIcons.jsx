c7c058412aa3321485b9961e623510dcce15dcca
Abhiraj412 <abhangparth@gmail.com>
2024-04-01 18:42:24 +0530

    commit

--- src/subComponents/SocialIcons.jsx
+++ src/subComponents/SocialIcons.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import React from "react";
 // import { NavLink } from 'react-router-dom'
 import styled from "styled-components";
-import { Facebook, Github, Twitter, YouTube } from "../components/AllSvgs";
+import { LinkedIn, Github, Twitter, LeetCode } from "../components/AllSvgs";
 import { DarkTheme } from "../components/Themes";
 
 const Icons = styled.div`
@@ -40,7 +40,7 @@ const SocialIcons = (props) => {
           rel="noreferrer"
           style={{ color: "inherit" }}
           target="_blank"
-          href={"https://github.com/codebucks27"}
+          href={"https://github.com/Parth0124"}
         >
           <Github
             width={25}
@@ -58,7 +58,7 @@ const SocialIcons = (props) => {
           rel="noreferrer"
           style={{ color: "inherit" }}
           target="_blank"
-          href={"https://twitter.com/code_bucks"}
+          href={"https://twitter.com/Parth010504"}
         >
           <Twitter
             width={25}
@@ -76,9 +76,9 @@ const SocialIcons = (props) => {
           rel="noreferrer"
           style={{ color: "inherit" }}
           target="_blank"
-          href={"https://facebook.com/codebucks27"}
+          href={"https://www.linkedin.com/in/parth-abhang-76619025b/"}
         >
-          <Facebook
+          <LinkedIn
             width={25}
             height={25}
             fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
@@ -94,9 +94,9 @@ const SocialIcons = (props) => {
           rel="noreferrer"
           style={{ color: "inherit" }}
           target="_blank"
-          href={"https://youtube.com"}
+          href={"https://leetcode.com/zoro3/"}
         >
-          <YouTube
+          <LeetCode
             width={25}
             height={25}
             fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
