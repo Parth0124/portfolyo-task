46ae13fc3ef6acf1bb7d69b3662a44ddf9c411ca
Abhiraj412 <abhangparth@gmail.com>
2024-04-02 11:48:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from "styled-components";
 import { lightTheme } from "./components/Themes";
 import { AnimatePresence } from "framer-motion";
 import GlobalStyle from "./globalStyles";
+import { StyleSheetManager } from "styled-components";
 
 //Components
 import Main from "./components/Main";
@@ -13,18 +14,14 @@ import MySkillsPage from "./components/MySkillsPage";
 function App() {
   const location = useLocation();
   return (
-    <>
+    <StyleSheetManager
+      shouldForwardProp={(prop) => prop !== "top" && prop !== "left"}
+    >
       <GlobalStyle />
 
       <ThemeProvider theme={lightTheme}>
-
-        
-        <AnimatePresence mode='wait'>
-          
-
-          <Routes key={location.pathname} location={location} >
-            
-
+        <AnimatePresence mode="wait">
+          <Routes key={location.pathname} location={location}>
             <Route path="/" element={<Main />} />
 
             <Route path="/about" element={<AboutPage />} />
@@ -32,12 +29,12 @@ function App() {
             <Route path="/work" element={<WorkPage />} />
 
             <Route path="/skills" element={<MySkillsPage />} />
-            
+
             <Route path="*" element={<Main />} />
           </Routes>
         </AnimatePresence>
       </ThemeProvider>
-    </>
+    </StyleSheetManager>
   );
 }
 
