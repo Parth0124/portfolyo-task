ac15104c4817aa28eefcb2cf029fbc47906c236e
Abhiraj412 <abhangparth@gmail.com>
2024-04-05 02:43:17 +0530

    projects fetched

--- src/components/WorkPage.jsx
+++ src/components/WorkPage.jsx
@@ -11,7 +11,8 @@ import { DarkTheme } from "./Themes";
 
 const Box = styled.div`
   background-color: ${(props) => props.theme.body};
-  height: 400vh;
+  /* Removed fixed height and added min-height */
+  min-height: 100vh;
   position: relative;
   display: flex;
   align-items: center;
@@ -41,36 +42,23 @@ const Rotate = styled.span`
 
 const WorkPage = () => {
   const [projectData, setProjectData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      // Here, I've directly used the provided dataset instead of fetching from the API
-      const data = {
-        success: true,
-        user: {
-          // ... (other user data)
-          projects: [
-            {
-              liveurl: "#",
-              githuburl: "#",
-              title: "Project 14",
-              sequence: 14,
-              image: {
-                public_id: "1706285511679-xe7r9t",
-                url: "https://portfolio-image-store.s3.ap-south-1.amazonaws.com/1706285511679-xe7r9t",
-              },
-              description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-              techStack: ["Reactjs ", " Nextjs ", " Mern ", " CSS"],
-              _id: "65b3d9c8d017f6b49c778ca7",
-              enabled: true,
-            },
-            // ... (other projects)
-          ],
-        },
-      };
-
-      setProjectData(data.user.projects);
+      try {
+        const response = await fetch('https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setProjectData(data.user.projects);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
     };
 
     fetchData();
@@ -84,21 +72,29 @@ const WorkPage = () => {
         <PowerButton />
 
         <Main>
-          <div className="fixed top-32 left-1/4 flex flex-col gap-8">
-            {projectData.map((project) => (
-              <ProjectCard
-                key={project._id}
-                name={project.title}
-                img={project.image.url}
-                tech={project.techStack}
-                detail={project.description}
-                liveUrl={project.liveurl}
-                github={project.githuburl}
-                enabled={project.enabled}
-                sequence={project.sequence}
-              />
-            ))}
-          </div>
+          {loading && (
+            <div className="text-center mt-8">Loading projects...</div>
+          )}
+          {error && (
+            <div className="text-center mt-8">Error: {error.message}</div>
+          )}
+          {!loading && !error && projectData && (
+            <div className="fixed top-32 left-1/4 flex flex-col gap-8">
+              {projectData.map((project) => (
+                <ProjectCard
+                  key={project._id}
+                  name={project.title}
+                  img={project.image.url}
+                  tech={project.techStack}
+                  detail={project.description}
+                  liveUrl={project.liveurl}
+                  github={project.githuburl}
+                  enabled={project.enabled}
+                  sequence={project.sequence}
+                />
+              ))}
+            </div>
+          )}
         </Main>
 
         <Rotate>
